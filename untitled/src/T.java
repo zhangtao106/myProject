ec2a9117663cf8804b289aa0829da6ddb518d947
zhangtao106 <389417703@QQ.COM>
2019-08-03 18:08:49 +0800

    2

---
+++ untitled/src/T.java
@@ -0,0 +1,6 @@
+public class T {
+    public static void main(String[] args) {
+        String str = "http://ts.21cn.com/news/client/2019/0422/14/33631473.shtml";
+        System.out.println(str.replace("client","a"));
+    }
+}
